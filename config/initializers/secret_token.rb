65720d57cca62a6b2f2a2876b2ee4e86f31cfa9f
Randy Villanueva <randyv12@hotmail.com>
2013-07-22 22:14:21 -0700

    initial commit of base code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareviewServer::Application.config.secret_token = '3d18d4a924048ead9535629e445f0180b98d612a98a52acb3300b97b60bfe9a3584661b96fd56ab56d9785779ddfd266850aa8edb2d5fdd6dec562811e7f2738'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShareviewServer::Application.config.session_store :cookie_store, key: '_shareview-server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShareviewServer::Application.config.session_store :active_record_store
